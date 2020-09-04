43510167efd9975bdda029f20a587ac9cf3eaf53
Joenel Cardinal <jcardinal@topjuan-tech.com>
2020-09-04 12:44:29 +0800

    add selectedIndex

--- src/Selector.js
+++ src/Selector.js
@@ -1,54 +1,49 @@
 import React, { Component } from 'react'
-import { FlatList, TouchableOpacity, View } from 'react-native'
+import { FlatList, TouchableOpacity, View, Text } from 'react-native'
 import PropTypes from 'prop-types'
 
 class Selector extends Component {
-  regions = this.props.data
+  data = this.props.data
+
   state = {
-    data: this.regions
+    data: this.data,
+    selectedIndex: []
   }
 
-  multiple = (id) => {
+  selection = (id) => {
+    let selectedIndex = []
     this.state.data.map((item, index) => {
-      // console.log(item[index])
+      this.props.multiple == false ? item.active = false : null
       index == id ? item.active = !item.active : item
+      item.active == true ? selectedIndex.push(index) : selectedIndex.slice(index, 1)
     })
-    this.setState({ data: this.state.data })
-  }
-
-  single = (id) => {
-    this.state.data.map((item, index) => {
-      // console.log(item[index])
-      item.active = false
-      index == id ? item.active = !item.active : item
-    })
-    this.setState({ data: this.state.data })
+    this.props.selectedIndex(selectedIndex)
+    this.setState({ data: this.state.data, selectedIndex: selectedIndex })
   }
 
   renderItem = ({ item, index }) => {
 
-    const { selectedColor, unselectedColor } = this.props
+    const {
+      selectedColor,
+      unselectedColor,
+      renderItem,
+    } = this.props
 
     return (
       <TouchableOpacity
         style={{ backgroundColor: item.active ? selectedColor : unselectedColor }}
-        onPress={() => {
-          this.props.multiple ?
-            this.multiple(index)
-            : this.single(index);
-        }}>
-        {this.props.renderItem(item, index)}
+        onPress={() => this.selection(index)}>
+        {renderItem(item, index)}
       </TouchableOpacity>
     )
   }
 
   render() {
     const {
-
     } = this.props
     return (
       <FlatList
-        data={this.regions}
+        data={this.data}
         extraData={this.state}
         renderItem={this.renderItem}
         keyExtractor={(key, index) => String(index)} />
@@ -56,18 +51,20 @@ class Selector extends Component {
   }
 }
 
+
 Selector.propTypes = {
   ...FlatList.propTypes,
   multiple: PropTypes.bool,
   selectedColor: PropTypes.string,
-  unselectedColor: PropTypes.string
+  unselectedColor: PropTypes.string,
+  renderItem: PropTypes.func.isRequired
 }
 
 
 Selector.defaultProps = {
   multiple: false,
   selectedColor: '#3535356E',
-  unselectedColor: 'transparent'
+  unselectedColor: 'transparent',
 }
 
 export default Selector;
